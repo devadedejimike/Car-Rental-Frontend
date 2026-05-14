158702a2a17a91fa38c8b3f1e3d36882dca52f5d
OutsydBaby <amichdot@gmail.com>
2026-05-14 13:22:37 +0100

    feat: implement auth context for global user state management with login and logout persistence

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import type { User } from "../types/auth";
+
+interface AuthContextType{
+    user: User | null,
+    token: string |null,
+    loading: boolean,
+    login: (token: string, user: User) => void,
+    logout: () => void
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({children}: {children: React.ReactNode}) => {
+    const [token, setToken] = useState<string | null>(null);
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const storedToken = localStorage.getItems('token');
+        const storedUser = localStorage.getItems('user');
+
+        if(storedToken && storedUser) {
+            setToken(storedToken)
+            setUser(JSON.parse(storedUser))
+        }
+        setLoading(false)
+    }, []);
+
+    const login = (token: string, user: User) => {
+        localStorage.setItem('token', token)
+        localStorage.setItem('user', JSON.stringify(user))
+        setToken(token)
+        setUser(user)
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        setToken(null)
+        setUser(null)
+    }
+    return (
+        <AuthContext.Provider
+            value={{
+                token, user, loading, login, logout
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if(!context) {
+        throw new Error('useAuth must be with AuthProvider')
+    }
+    return context;
+}
